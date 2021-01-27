402ee112bc4ff101d237c49a5160c96889083d39
ing181 <ingemar.ceicer@lerum.se>
2021-01-27 14:04:40 +0100

    new

---
+++ Om new/dynamic_memory/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+// Dynamisk minneshantering
+int main()
+{
+
+    int *talpekare; // kan innehålla adressen till en int
+                    // * betyder att datatypen är "adressen till en int"
+    talpekare = new int; // Alokeras, nokas, skapas plats på datorns RAM-minne
+                         // där vi kan spara en int
+    cout << "talpekare (adress till en int) " << talpekare << endl;
+
+    cin >> *talpekare;  // * betyder "Det som finns på adressen som finns i talpekare"
+
+    cout << "innehållet på adressen talpekare " << *talpekare << endl; // Skriver ut "Det som finns på adressen" i talpekare
+
+    int tal = *talpekare;
+    cout << "tal " << tal << endl;
+    cout << "adressen till tal " << &tal << endl; // & ger oss adressen
+
+
+    delete talpekare; // Lämnar tillbaka utrymmet på RAM-minnet
+                      // till operativsystemet.
+
+
+
+    return 0;
+}
+
