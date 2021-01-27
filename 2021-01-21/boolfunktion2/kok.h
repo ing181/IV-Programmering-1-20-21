bff1dba6ddcc017404847404b89e77a487cf5cb7
ing181 <ingemar.ceicer@lerum.se>
2021-01-27 11:47:06 +0100

    Funktion i headerfil v2.0 h-files

---
+++ 2021-01-21/boolfunktion2/kok.h
@@ -0,0 +1,6 @@
+#ifndef KOK_H_INCLUDED
+#define KOK_H_INCLUDED
+
+bool IsBoiling(int);
+
+#endif // KOK_H_INCLUDED
---
+++ Funktion i headerfil v2.0/V2.0/funktioner.h
@@ -0,0 +1,9 @@
+#ifndef FUNKTIONER_H_INCLUDED
+#define FUNKTIONER_H_INCLUDED
+#include <iostream>
+#include <string>
+using namespace std;
+// Funktionsdeklaration
+void skrivUt(string);
+
+#endif // FUNKTIONER_H_INCLUDED
